a07d1163200c509e046bef20ceec36d2d07fc895
Guillaume Rossignol <guillaume.rossignol@wizacha.com>
2014-11-20 06:38:58 +0100

    Added config file

---
+++ DependencyInjection/WizachaUniversignExtension.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the WizachaUniversign bundle.
+ *
+ * (c) Wizacha <dev-team@wizacha.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Wizacha\UniversignBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface
+
+class WizachaUniversignExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('service.yml');
+
+    }
+}
